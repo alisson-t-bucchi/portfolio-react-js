49283ded3743115d531b3766ed0c3db7ee470baa
alisson-t-bucchi <alisson.bucchi@outlook.com>
2025-03-02 18:27:42 +0000

    adding image on HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,6 +10,10 @@ export default function HeroSection({content: {title, role}}) {
         <div className="flex flex-col gap-4 justify-center items-center lg:items-start">
             <h2 className="text-4xl font-bold">{title}</h2>
             <h3 className="text-2xl font-bold">{role}</h3>
+            <img className="w-60 rounded-full md:hidden border-4" 
+            src={WebPhoto} 
+            alt="Alisson" 
+            />
 
             {/* <button className=" mt-4 bg-blue-600 text-white px-6 py-2 rounded-lg">
               Contact me
