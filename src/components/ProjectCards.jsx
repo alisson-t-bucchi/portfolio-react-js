2d3df94e4f7527bc52c26f069cf2fbe529c58ff8
alisson-t-bucchi <alisson.bucchi@outlook.com>
2025-04-11 14:31:30 +0100

    adjusting viewport of animations in ProjectCard components

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -60,6 +60,7 @@ const qaProjects = [
         image: MagentoPage,
         title: "Playwright test suite for Magento Page.",
         description: "E2E automation tests for Magento Page in JS",
+        technologies: "Playright.",
         video: ComingSoon,  //criar video
         code: "https://github.com/alisson-t-bucchi/magento-playwright-js.git", 
     },
@@ -151,10 +152,10 @@ export default function Cards() {
         <section id="QAprojects" /* </>className="shadow-lg rounded-lg p-4" */>
             <h3 className="text-3xl font-bold text-center mb-4 py-4">✅ QA Projects</h3>
             <motion.div
-            className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6 mb-4"
+            className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6 mb-4 sm:grid-cols-1 gap-6"
             initial="offscreen"
             whileInView="onscreen"
-            viewport={{ amount: 0.5, once: true }}
+            viewport={{ amount: 0.3, once: true }}
             variants={containerVariants}
             >
                 {qaProjects.map((project) => (
@@ -179,10 +180,10 @@ export default function Cards() {
         <section id="AIprojects">
             <h3 className="text-3xl font-bold text-center mb-4 py-4">🤖 IA Projects</h3>
             <motion.div
-            className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6 mb-4"
+            className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6 mb-4 sm:grid-cols-1 gap-6"
             initial="offscreen"
             whileInView="onscreen"
-            viewport={{ amount: 0.5, once: true }}
+            viewport={{ amount: 0.3, once: true }}
             variants={containerVariants}
             >
                 {iaProjects.map((project) => (
