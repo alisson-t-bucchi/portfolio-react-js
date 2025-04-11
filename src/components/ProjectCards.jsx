b89921bb98cf4b656b26e0f7c992aa2eeb1a3b0a
alisson-t-bucchi <alisson.bucchi@outlook.com>
2025-04-11 13:15:50 +0100

    including motion effects to ProjectCards components

--- src/components/ProjectCards.jsx
+++ src/components/ProjectCards.jsx
@@ -1,3 +1,5 @@
+import { motion } from "motion/react"
+
 import ComingSoon from "../assets/images/coming-soon.webp"
 //
 import MagentoPage from "../assets/images/magento-page.webp"
@@ -18,15 +20,48 @@ import PizzaPricePredictorAppVideo from "../assets/videos/pizza-price-predictor-
 import AIChefDeCuisine from "../assets/images/ai-chef-de-cuisine.webp"
 import AIChefDeCuisineVideo from "../assets/videos/ai-chef-de-cuisine.mp4"
 
+const containerVariants = {
+    offscreen: {
+      opacity: 0,
+      y: 50,
+    },
+    onscreen: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        staggerChildren: 0.2, // Delay entre os filhos
+        type: "spring",
+        bounce: 0.4,
+        duration: 0.8,
+      },
+    },
+  }
+
+const cardVariants = {
+    offscreen: {
+        opacity: 0,   
+        y: 200, 
+    },
+    onscreen: {
+        opacity: 1, 
+        y: 0,
+        rotate: 0,
+        transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8,
+        },
+    },
+}
+
 const qaProjects = [
     {
         id: 1,
         image: MagentoPage,
         title: "Playwright test suite for Magento Page.",
         description: "E2E automation tests for Magento Page in JS",
-        technologies: "Plawright.", 
         video: ComingSoon,  //criar video
-        code: "https://github.com/alisson-t-bucchi/magento-playwright-js.git"
+        code: "https://github.com/alisson-t-bucchi/magento-playwright-js.git", 
     },
     {
         id: 2,
@@ -93,7 +128,11 @@ const CardsItems = ({image, title, description, technologies, video, code}) => {
         <img src={image} alt="Project" className="rounded-md" />
         <h4 className="mt-4 text-xl font-bold">{title}</h4>
         <p className="text-l">{description}</p>
-        <p className="text-l hover:text-red-600 transition-colors transform hover:scale-100">Tech stack: {technologies}</p>
+        {technologies && (
+            <p className="text-l hover:text-red-600 transition-colors transform hover:scale-100">
+                Tech stack: {technologies}
+            </p>
+        )}
         <div className="flex space-x-4 mt-4">
             <a href={video} target="_blank" rel="noopener noreferrer" className="bg-white px-4 py-1 rounded-lg border-2">
                 Live Preview
@@ -106,40 +145,62 @@ const CardsItems = ({image, title, description, technologies, video, code}) => {
     )
 }
 
-export default function Cards() {
+export default function Cards() {      
     return(
     <>
-        <section id="QAprojects" className="shadow-lg rounded-lg p-4 border-1 border-black">
+        <section id="QAprojects" /* </>className="shadow-lg rounded-lg p-4" */>
             <h3 className="text-3xl font-bold text-center mb-4 py-4">✅ QA Projects</h3>
-            <div className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6 mb-4">
+            <motion.div
+            className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6 mb-4"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ amount: 0.5, once: true }}
+            variants={containerVariants}
+            >
                 {qaProjects.map((project) => (
-                    <CardsItems
+                    <motion.div
                     key={project.id}
+                    variants={cardVariants}
+                    >
+                    <CardsItems
                     image={project.image}
                     title={project.title}
                     description={project.description}
                     technologies={project.technologies}
                     video={project.video}
-                    code={project.code} />
-                    ))}
-            </div> 
+                    code={project.code}
+                    icons={project.icons} />
+                    </motion.div>
+                    ))} 
+            </motion.div>
         </section>
    
         
-        <section id="AIprojects" className="shadow-lg rounded-lg p-4 mt-12 border-1 border-black ">
+        <section id="AIprojects">
             <h3 className="text-3xl font-bold text-center mb-4 py-4">🤖 IA Projects</h3>
-            <div className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6">
+            <motion.div
+            className="grid lg:grid-cols-3 lg:gap-6 md:grid-cols-2 md:gap-6 mb-4"
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ amount: 0.5, once: true }}
+            variants={containerVariants}
+            >
                 {iaProjects.map((project) => (
-                    <CardsItems
-                    key={project.id}
-                    image={project.image}
-                    title={project.title}
-                    description={project.description}
-                    technologies={project.technologies}
-                    video={project.video}
-                    code={project.code} />
+                      <motion.div
+                      key={project.id}
+                      variants={cardVariants}
+                      >
+                      <CardsItems
+                      image={project.image}
+                      title={project.title}
+                      description={project.description}
+                      technologies={project.technologies}
+                      video={project.video}
+                      code={project.code}
+                      icons={project.icons} />
+                      </motion.div>
                     ))}
-            </div>
+            </motion.div>
         </section>
     </>
     )
