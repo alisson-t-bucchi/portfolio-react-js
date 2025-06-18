975cbeb4b39f8244905e1781ae242feafd4aaba0
alisson-t-bucchi <alisson.bucchi@outlook.com>
2025-06-18 13:03:09 +0100

    some adjust to toggle menu in mobile screen

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -8,26 +8,41 @@ import ScrollDinamicIndicator from "../components/ScrollDinamicIndicator";
 
 export default function Layout({ children }) {
     const [menuVisible, setMenuVisible] = useState(false);
+    const [projectsOpen, setProjectsOpen] = useState(false);
   
     const toggleMenu = () => {
       setMenuVisible(!menuVisible);
+      setProjectsOpen(false);
     };
 
+      const toggleProjects = () => {
+        setProjectsOpen(!projectsOpen);
+    };
 
     return (
     <>
         <nav className="scroll-smooth py-4 flex justify-between items-center lg:px-12">
             <h1 className="text-3xl font-bold">My Portfolio</h1>
+
             <div className="lg:hidden" >
                 <FontAwesomeIcon icon={faBars} size="2x" onClick={toggleMenu} /> 
             </div>
+
             <div className={`${menuVisible ? "block" : "hidden"} space-x-6 font-bold text-2xl lg:flex flex-col lg:flex-row absolute lg:static top-16 left-0 w-full lg:w-auto bg-lime-100 lg:bg-transparent shadow-lg lg:shadow-none p-4 lg:p-0` }>
                 <a href="#about" className="hover:text-white transition-colors duration-150 transform hover:scale-125">About</a>
+                 
                  <div className="relative group">
-                    <button className="hover:text-white transition-colors duration-150 transform hover:scale-125">
+                    <button 
+                    className="hover:text-white transition-colors duration-150 transform hover:scale-125"
+                    onClick={toggleProjects}    
+                    >
                         Projects
                     </button>
-                    <div className="hidden group-hover:block absolute lg:left-1/2 lg:-translate-x-1/2 mt-2 bg-lime-200 rounded-md shadow-lg p-4 space-y-2 text-xl text-black min-w-[200px]">
+                    <div className={`${projectsOpen ? "block" : "hidden"} 
+                        lg:group-hover:block 
+                        absolute z-10 mt-2 bg-lime-100 rounded-md shadow-lg p-4 space-y-2 text-xl text-black min-w-[200px]
+                        lg:left-1/2 lg:-translate-x-1/2
+                        `}>
                         <a href="#qa" className="block hover:bg-lime-300 px-3 py-2 rounded">QA Projects</a>
                         <a href="#ai-dataviz" className="block hover:bg-lime-300 px-3 py-2 rounded">AI & Data Visual</a>
                         <a href="#fullstack" className="block hover:bg-lime-300 px-3 py-2 rounded">Full-Stack Apps</a>
@@ -62,6 +77,5 @@ export default function Layout({ children }) {
         </footer>
         <SpeedInsights />
     </>
-  )
-}
+)}
 
