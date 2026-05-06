4c4ede8eca00a26d098e6bf4b8e89ac368ffba34
alisson-t-bucchi <alisson.bucchi@outlook.com>
2026-05-06 20:08:39 -0300

    some layout for icon in About Me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -11,7 +11,7 @@ export default function AboutMe() {
         <div className="flex justify-center">
           <DotLottieReact
             src="https://lottie.host/e27b8e85-6476-4ff8-955c-e681beffa455/ii4uA4xPmk.lottie"
-            className="w-full max-w-[400px] h-auto"
+            className="w-full max-h-full h-auto"
             autoplay
             loop
           />
